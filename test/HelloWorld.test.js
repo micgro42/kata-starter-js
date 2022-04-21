7dab3369026a75d503e153b47fbd74de1967d067
Michael Große <michael.grosse@wikimedia.de>
2022-04-21 13:17:27 +0200

    Add template for parametrized test
    Many kata tests only differ in the input and expected values, so using a
    parametrized test reduces the overhead of writing the test. This allows
    for more time actually writing the kata code

--- test/HelloWorld.test.js
+++ test/HelloWorld.test.js
@@ -1,3 +1,11 @@
 it('has a working test', () => {
   expect(true).toBe(true);
 });
+
+/*
+it.each([
+  [1, 2, 3],
+])('adds %i to %i to equal %i', (a, b, expected) => {
+  expect(a + b).toBe(expected);
+});
+*/
